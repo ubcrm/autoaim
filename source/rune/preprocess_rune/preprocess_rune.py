6b0deb4267cb55b831c429b1ccde9b9628bc056a
Pedram Amani <amani.pedram@gmail.com>
2019-12-01 11:25:44 -0800

    added docstring in PreprocessRune

--- source/rune/preprocess_rune/preprocess_rune.py
+++ source/rune/preprocess_rune/preprocess_rune.py
@@ -4,12 +4,17 @@ from pathlib import Path
 from source.common.module import Module
 
 
-class Preprocess(Module):
+class PreprocessRune(Module):
     def __init__(self, state=None):
         self.working_dir = Path(os.path.dirname(os.path.abspath(__file__)))
         super().__init__(self.working_dir, state=state)
 
     def process(self, frame, rune_center):
+        """
+        :param frame: power rune image to be cropped
+        :param rune_center: center coordinate of power rune in the frame
+        :return: tuple of cropped image, amount to shift frame left, top by
+        """
         frame = frame.copy()
         dims = frame.shape[:2]
         radius = self.properties["scaling"] * dims[0] / 2
--- source/rune/rune.py
+++ source/rune/rune.py
@@ -2,14 +2,14 @@ import os
 from pathlib import Path
 from source.common.module import Module
 from source.rune.predict_target.predict_target import PredictTarget
-from source.rune.preprocess.preprocess import Preprocess
+from source.rune.preprocess_rune.preprocess_rune import PreprocessRune
 
 
 class Rune(Module):
     def __init__(self, state=None):
         self.working_dir = Path(os.path.dirname(os.path.abspath(__file__)))
         super().__init__(self.working_dir, state=state)
-        self.preprocess = Preprocess()
+        self.preprocess = PreprocessRune()
         self.predict_target = PredictTarget()
         self.rune_center = self.properties["rune_center"]
 
