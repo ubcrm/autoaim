e24da0dfe48f5316f8138a454bf2f3111351ffff
Connor <connor.cahoon@gmail.com>
2019-12-10 19:45:12 -0800

    changed how webcam input is read in other main files fixed rune runtime bug

--- source/rune/__main__.py
+++ source/rune/__main__.py
@@ -3,7 +3,7 @@ import cv2
 from imutils.video import VideoStream
 import argparse
 
-from rune.rune import Rune
+from source.rune.rune import Rune
 
 
 def display_target(frame, point):
@@ -13,47 +13,33 @@ def display_target(frame, point):
     cv2.imshow('Press q to quit', frame)
 
 
-def run_video(video_path):
+def run_video(capture):
     rune = Rune()
-    cap = cv2.VideoCapture(video_path)  # load video
-    ret, frame = cap.read()  # ret = 1 if the video is captured; frame_bw is the image in blue, green, red
+    ret, frame = capture.read()  # ret = 1 if the video is captured; frame_bw is the image in blue, green, red
     if not ret:
-        raise FileNotFoundError("video at " + str(video_path) + " not found")
+        raise FileNotFoundError("input not found")
     while ret:
         prediction = rune.process(frame)
         display_target(frame, prediction)
         if cv2.waitKey(1) & 0xFF == ord('q'):  # press q to quit
             break
-        ret, frame = cap.read()  # get next frame
-    cap.release()
+        ret, frame = capture.read()  # get next frame
+    capture.release()
     cv2.destroyAllWindows()
-
-
-def run_live():
-    rune = Rune()
-    cap = VideoStream(src=0).start()
-    frame = cap.read()
-
-    while frame is not None:
-        prediction = rune.process(frame)
-        display_target(frame, prediction)
-        if cv2.waitKey(1) & 0xFF == ord('q'):  # press q to quit
-            break
-        frame = cap.read()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--mode', default="camera",
-                        help="Sets the mode of the classifier. Options: video, camera")
+                        help="Sets the mode of the classifier. Options: video, webcam")
     parser.add_argument("-v", "--video", help="Path to video")
 
     args = vars(parser.parse_args())
 
     if args["mode"] == "video" and args["video"]:
-        run_video(video_path=args["video"])
-    elif args["mode"] in ["webcam", "pi", "camera", "live"]:
-        run_live()
+        run_video(capture=cv2.VideoCapture(args["video"]))
+    elif args["mode"] in ["webcam", "camera", "live"]:
+        run_video(capture=cv2.VideoCapture(0))
     else:
         print("No valid mode specified, Exiting.")
         exit(1)
