1f2246ce3de5aafa73cb9a3fc365fc3fbe2c3cec
Josh Marangoni <joshua.marangoni@gmail.com>
2020-03-15 12:18:09 -0700

    Fix bug

--- source/__main__.py
+++ source/__main__.py
@@ -38,7 +38,7 @@ def run(panel_predictor, gimbal_angle_finder, capture, display=False):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('c', "--camera", default="raspberry", help="Which camera to use, e.g. raspberry, webcam")
+    parser.add_argument('-c', "--camera", default="raspberry", help="Which camera to use, e.g. raspberry, webcam")
     parser.add_argument('-i', '--framework', default="opencv",
                         help="Specifies which framework to use as inference, e.g. opencv, tensorflow")
     parser.add_argument('-s', '--show', type=bool, help='Conditonal for displaying frame', default=False)
