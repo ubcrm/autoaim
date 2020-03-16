30e138002a041c1e21087402d142fccd8fa72da0
Josh Marangoni <joshua.marangoni@gmail.com>
2020-03-16 13:24:10 -0700

    change fov to 170, made variables more clear, changed polarity of delta

--- source/gimbal_angle_finder/gimbal_angle_finder.py
+++ source/gimbal_angle_finder/gimbal_angle_finder.py
@@ -11,10 +11,10 @@ class GimbalAngleFinder(Module):
 
     # Processes screen coords and frame and converts them to a set of angles
     def process(self, x, y, frame_dims):
-        adj_x = x - (frame_dims[0] / 2)
-        adj_y = y - (frame_dims[1] / 2)
-        horiz_angle = math.radians((adj_x / (frame_dims[0] / 2)) * self.properties["horiz_fov"])
-        vert_angle = math.radians((adj_y / (frame_dims[1] / 2)) * self.properties["vert_fov"])
+        adjusted_x = (frame_dims[0] / 2) - x
+        adjusted_y = (frame_dims[1] / 2) - y
+        horiz_angle = math.radians((adjusted_x / (frame_dims[0] / 2)) * self.properties["horiz_fov"])
+        vert_angle = math.radians((adjusted_y / (frame_dims[1] / 2)) * self.properties["vert_fov"])
         return horiz_angle, vert_angle
 
 
