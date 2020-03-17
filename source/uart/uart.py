623c7854ae355251d8d9bdaa0cd18751ef0c570d
JoshMarangoni <joshua.marangoni@mimik.com>
2020-03-16 20:06:05 -0700

    try different port on pi

--- source/uart/uart.py
+++ source/uart/uart.py
@@ -9,7 +9,14 @@ class Uart(Module):
 	def __init__(self, parent=None, state=None):
 		self.working_dir = Path(os.path.dirname(os.path.abspath(__file__)))
 		super().__init__(self.working_dir, parent=parent, state=state)
-		self.ser = serial.Serial("/dev/ttyS0", 115200)
+		#self.ser = serial.Serial("/dev/ttyS0", 115200)
+		self.ser = serial.Serial(
+			port='/dev/serial0' ,\
+			baudrate=115200,\
+			parity=serial.PARITY_NONE,\
+			stopbits=serial.STOPBITS_ONE,\
+			bytesize=serial.EIGHTBITS,\
+				timeout=0)
 
 	def read_buffer(self):
 		received_data = self.ser.read()
