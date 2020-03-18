56a64445011466f1c8373d7e3f960a6ad25b9579
JoshMarangoni <joshua.marangoni@mimik.com>
2020-03-17 23:51:34 -0700

    working?

--- source/__main__.py
+++ source/__main__.py
@@ -38,7 +38,6 @@ def run(panel_predictor, gimbal, uart, capture, display=True):
             #next_angle = current_angle + gimbal.process(target[0], target[1], frame_shape)
             target, distance, cumulative_confidence = prediction
             delta_angle = gimbal.process(target[0], target[1], frame_shape)
-            print(delta_angle)
             uart.send_hex(int(delta_angle[0]))
         
         if display:  #TO-DO
