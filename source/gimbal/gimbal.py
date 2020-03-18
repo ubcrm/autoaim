3b25fad8e4a64e802ebaa9a12606070861b22ea4
JoshMarangoni <joshua.marangoni@mimik.com>
2020-03-18 00:02:42 -0700

    truncate angle

--- source/gimbal/gimbal.py
+++ source/gimbal/gimbal.py
@@ -11,10 +11,17 @@ class Gimbal(Module):
 		super().__init__(self.working_dir, parent=parent, state=state)
 		self.uart = Uart()
 	
+	def truncate_angle(self, angle):
+		if angle > 180:
+			return 180
+		elif angle < -180:
+			return -180
+		return angle
+
 	# Processes screen coords and frame and converts them to a set of angles
 	def process(self, x, y, frame_dims):
 		adjusted_x = (frame_dims[0] / 2) - x
 		adjusted_y = (frame_dims[1] / 2) - y
 		horiz_angle = (adjusted_x / (frame_dims[0] / 2)) * self.properties["horiz_fov"]
 		vert_angle = (adjusted_y / (frame_dims[1] / 2)) * self.properties["vert_fov"]
-		return horiz_angle, vert_angle
+		return self.truncate_angle(horiz_angle), self.truncate_angle(vert_angle)
