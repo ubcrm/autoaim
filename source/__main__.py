3bb496e639a3a5997a94c7ddf20cadc6d8c32fa3
JoshMarangoni <joshua.marangoni@mimik.com>
2020-03-17 23:12:50 -0700

    hack

--- source/__main__.py
+++ source/__main__.py
@@ -24,24 +24,27 @@ def display_frame(frame, target=None):
         #cv2.putText(frame, str(int(distance * 100)) + "m", target, cv2.FONT_HERSHEY_PLAIN, 0.9, (255, 255, 255))
     cv2.imshow('Press q to quit', frame)
 
-def run(panel_predictor, gimbal, uart, capture, display=True):
+#def run(panel_predictor, gimbal, uart, capture, display=True):
+def run(panel_predictor, capture):
     ret, frame = capture.read()  # ret = 1 if the video is captured; frame is the image in blue, green, red
     if not ret:
         raise FileNotFoundError("input not found")
     while ret:
         frame = cv2.pyrDown(frame)
-        frame_shape = frame.shape[:2]
+
+        #frame_shape = frame.shape[:2]
+        
         #target, distance, cumulative_confidence = panel_predictor.process(frame)
         target = panel_predictor.process(frame)
         #current_angle = uart.read_hex()
         #next_angle = current_angle + gimbal.process(target[0], target[1], frame_shape)
-        try:
-            delta_angle = gimbal.process(target[0], target[1], frame_shape)
-            uart.send_hex(delta_angle)
-        except:
-            print("delta angle failure")
-            print("target0: "+ target[0])
-            print("target1: " + target[1])
+        #try:
+            #delta_angle = gimbal.process(target[0], target[1], frame_shape)
+            #uart.send_hex(delta_angle)
+        #except:
+            #print("delta angle failure")
+            #print("target0: "+ target[0])
+            #print("target1: " + target[1])
         
         #if display:  #TO-DO
         #display_frame(frame, distance, cumulative_confidence, delta_angle, target)
@@ -67,6 +70,7 @@ if __name__ == "__main__":
         #video_stream = VideoStream(usePiCamera=True).start()
 
     panel_predictor = PanelPredictor(state={"framework": args["framework"]})
-    gimbal = Gimbal()
-    uart = Uart()
-    run(panel_predictor, gimbal, uart, cv2.VideoCapture(0), args["show"])
+    #gimbal = Gimbal()
+    #uart = Uart()
+    #run(panel_predictor, gimbal, uart, cv2.VideoCapture(0), args["show"])
+    run(panel_predictor, cv2.VideoCapture(0))
