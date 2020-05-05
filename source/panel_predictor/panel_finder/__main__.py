bed109c032b11dc8c97e5689616d0b2298760e38
Josh Marangoni <joshua.marangoni@gmail.com>
2020-05-05 12:56:12 -0700

    Display confidence panel finder + pause video with spacebar

--- source/panel_predictor/panel_finder/__main__.py
+++ source/panel_predictor/panel_finder/__main__.py
@@ -3,25 +3,34 @@ import argparse
 import cv2
 
 
-def display_frame(frame, panel=None):
+def display_frame(frame, confidence=None, target=None):
     # Display the resulting image
-    if panel is not None:
-        cv2.circle(frame, (panel[0]["x_center"], panel[0]["y_center"]), 3, (0, 255, 0), -1)
+    if target is not None:
+        cv2.circle(frame, (target["x_center"], target["y_center"]), 6, (0, 255, 0), -1)
+        cv2.putText(frame, str(int(confidence * 100)) + "%", (target["x_center"], target["y_center"]), 
+                cv2.FONT_HERSHEY_PLAIN, 1.5, (255, 255, 255))
     cv2.imshow('Press q to quit', frame)
 
 
 def run_video(framework, capture):
     ret, frame = capture.read()
+    pause_flag = False
     if not ret:
         raise FileNotFoundError("input not found")
     while ret:
-        panel = panel_finder.process(frame)
-        display_frame(frame, panel)
+        confidence, target, _ = panel_finder.process(frame)
+        display_frame(frame, confidence, target)
+
+        if cv2.waitKey(1) & 0xFF == ord(' '):
+            pause_flag = not(pause_flag)
+
+        if not pause_flag:
+            ret, frame = capture.read()
 
         if cv2.waitKey(1) & 0xFF == ord('q'):  # press q to quit
             break
         # get next frame
-        ret, frame = capture.read()
+        
     capture.release()
     cv2.destroyAllWindows()
 
--- source/panel_predictor/panel_finder/panel_finder.py
+++ source/panel_predictor/panel_finder/panel_finder.py
@@ -62,4 +62,4 @@ class PanelFinder(Module):
                 if confidence > best_pair[2]:
                     best_pair = (leds[i], leds[i + 1], confidence)
 
-            return combined_panel(best_pair[0], best_pair[1]), best_pair[2]
+            return confidence, combined_panel(best_pair[0], best_pair[1]), best_pair[2]
