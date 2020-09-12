f21801f335c9c4399f8e980f95af23f3c2e19336
jirm2019 <jachnoin2@gmail.com>
2020-09-12 14:16:32 -0700

    refactoring, mask module wip

--- autoaim/find_target/find_panel/mask/mask.py
+++ autoaim/find_target/find_panel/mask/mask.py
@@ -1,9 +1,22 @@
 import mask_config as CONFIG
-
+import cv2
+import numpy as np
 
 class Mask:
-    def __init__(self):
-        pass
+    def __init__(self, parent, **config):
+        self.wd = CONFIG.DIR
+        super().__init__(self.wd, parent, config)
+        frame_height = parent.robot_crop.config['dims'][1]
+        self.config['dilate'] = round(self.config['dilate_rel'] * frame_height)
+        self.config['morph'] = [round(val * frame_height) for val in self.config['morph_rel']]
 
-    def run(self):
-        pass
+    def run(self, roi):
+        """
+        :return: B&W led mask of ROI. Value of each pixel is 0 or 255.
+        """
+        mask = cv2.inRange(roi, tuple(self.config['range'][0]), tuple(self.config['range'][1]))
+        mask = cv2.dilate(mask, np.ones((self.config['dilate'], self.config['dilate'])))
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, tuple(self.config['morph']))
+        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+
+        return mask
