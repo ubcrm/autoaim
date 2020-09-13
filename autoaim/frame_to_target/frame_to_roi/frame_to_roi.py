65ab9de38e03b5bcead84a66a312e1191b42de83
pedramamani <amani.pedram@gmail.com>
2020-09-12 20:40:16 -0700

    fix debug_frame assignment in frame_to_roi.py

--- autoaim/frame_to_target/frame_to_roi/frame_to_roi.py
+++ autoaim/frame_to_target/frame_to_roi/frame_to_roi.py
@@ -3,11 +3,12 @@ import cv2
 
 
 def frame_to_roi(roi, debug_frame=None):
-    height, width = roi.shape[:2]
-    resize_dims = (round(height * CONFIG.SCALE), round(width * CONFIG.SCALE))
-
+    height, width, _ = roi.shape
+    resize_dims = (round(width * CONFIG.SCALE), round(height * CONFIG.SCALE))
     roi = cv2.resize(roi, resize_dims)
+
     if debug_frame is not None:
-        debug_frame[:] = cv2.resize(debug_frame, resize_dims)
+        debug_frame.resize(roi.shape, refcheck=False)
+        debug_frame[:] = roi
     return roi
 
--- autoaim/frame_to_target/roi_to_mask/roi_to_mask.py
+++ autoaim/frame_to_target/roi_to_mask/roi_to_mask.py
@@ -7,6 +7,7 @@ def roi_to_mask(roi, debug_frame=None):
     height = roi.shape[0]
     dilate_dim = round(CONFIG.DILATE_REL * height)
     morph_dims = (round(CONFIG.MORPH_REL[0] * height), CONFIG.MORPH_REL[1] * height)
+    print(dilate_dim)
 
     mask = cv2.inRange(roi, *CONFIG.RANGE)
     mask = cv2.dilate(mask, np.ones(dilate_dim, dilate_dim))
